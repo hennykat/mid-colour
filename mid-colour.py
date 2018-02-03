80430f8b9abb57330843b8aef913f0ea132f0b5a
Hennessy <hennykdev@gmail.com>
2018-02-02 19:52:04 -0600

    implemented actual program

--- mid-colour.py
+++ mid-colour.py
@@ -54,14 +54,14 @@ def not_divmod(div, rem, base):
 
 def hex_to_rgb(hex):
 	hex_base = 16
-
+	# break hex into parts
 	one_pt_one = int(hex.un[0], 16)
 	one_pt_two = int(hex.un[1], 16)
 	two_pt_one = int(hex.deux[0], 16)
 	two_pt_two = int(hex.deux[1], 16)
 	three_pt_one = int(hex.trois[0], 16)
 	three_pt_two = int(hex.trois[1], 16)
-
+	# reverse the divmod
 	red = not_divmod(one_pt_one, one_pt_two, hex_base)
 	green = not_divmod(two_pt_one, two_pt_two, hex_base)
 	blue = not_divmod(three_pt_one, three_pt_two, hex_base)
@@ -69,12 +69,48 @@ def hex_to_rgb(hex):
 	return RGBColour(red, green, blue)
 
 
-# test
-my_rgb = RGBColour(33, 223, 101)
-my_rgb.print()
-my_hex = rgb_to_hex(my_rgb)
-my_hex.print()
+def find_mid_rgb(rgb_one, rgb_two):
+	mid_r = int((rgb_one.r + rgb_two.r) / 2)
+	mid_g = int((rgb_one.g + rgb_two.g) / 2)
+	mid_b = int((rgb_one.b + rgb_two.b) / 2)
 
-new_rgb = hex_to_rgb(my_hex)
-new_rgb.print()
+	return RGBColour(mid_r, mid_g, mid_b)
 
+
+def hex_from_str(hex_str):
+	# split into bytes
+	split_hex = [hex_str[i : i + 2] for i in range(0, len(hex_str), 2)]
+	# check len
+	if len(split_hex) != 3:
+		return HexColour(0, 0, 0)
+	else:
+		return HexColour(split_hex[0], split_hex[1], split_hex[2])
+
+
+# actual program
+while(1):
+	in_one = input("input first hex (#xxxxxx): ")
+	in_two = input("input second hex (#xxxxxx): ")
+
+	clean_one = in_one.replace("#", "")
+	clean_two = in_two.replace("#", "")
+	# check len
+	hex_len = 6
+	if len(clean_one) != hex_len or len(clean_two) != hex_len:
+		print("bad input, try again")
+		continue
+	# create hex objs
+	hex_one = hex_from_str(clean_one)
+	hex_two = hex_from_str(clean_two)
+	# convert to rgb objs
+	rgb_one = hex_to_rgb(hex_one)
+	rgb_two = hex_to_rgb(hex_two)
+
+	mid_rgb = find_mid_rgb(rgb_one, rgb_two)
+	mid_hex = rgb_to_hex(mid_rgb)
+	print("mid hex is:")
+	mid_hex.print()
+
+	in_cont = input("would you like to go again (y or n): ")
+	if in_cont.lower() != 'y':
+		break
